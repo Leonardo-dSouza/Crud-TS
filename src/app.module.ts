9f14fd1dd92cf0edf7973ecfc304c582fafcefa7
leonardo-dsouza <dkaireturn@gmail.com>
2025-11-13 17:56:30 -0300

    remove old modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
-import { UsersModule } from './users/users.module';
-import { CountriesModule } from './countries/countries.module';
 import { CountryModule } from './country/country.module';
 import { ContinentModule } from './continent/continent.module';
 import { CityModule } from './city/city.module';
-import { ContinentsModule } from './continents/continents.module';
-import { CitiesModule } from './cities/cities.module';
-import { ContinentsModule } from './continents/continents.module';
-import { CitiesModule } from './cities/cities.module';
-import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [ItemsModule, UsersModule, CountriesModule, ContinentsModule, CitiesModule, CityModule, ContinentModule, CountryModule],
+  imports: [CityModule, ContinentModule, CountryModule],
   controllers: [AppController],
   providers: [AppService],
 })
